d975f23b331d21b1fabe570f431a866e4434dcb0
NISHANTH-777 <nisanthp,cs24@bitsathy.ac.in>
2025-02-12 15:43:41 +0530

    modified

--- src/App.jsx
+++ src/App.jsx
@@ -5,12 +5,12 @@ function App() {
   return (
    <div className="content">
     <center>
-    <h1>WELCOME TO YOUR PAGE </h1>
-    <p1>WE ARE GLAD TO INVITE YOU FOR OUR WEBSITE. <br /></p1>
-    <p2>PLEASE SIGN UP FOR FUTHER PROCESS. <br /></p2>
+    <h1 className='heading'>WELCOME TO YOUR PAGE </h1>
+    <p1 className='para'>WE ARE GLAD TO INVITE YOU FOR OUR WEBSITE. <br /><br /></p1>
+    <p2 className='parag'>PLEASE SIGN UP FOR FUTHER PROCESS. <br /><br /></p2>
     <form className="signup" >
-    <input type="text" placeholder='YOUR EMAIL ID' required/><br />
-    <input type="text" placeholder='YOUR PASSWORD' required /><br />
+    <input type="text" placeholder='YOUR EMAIL ID' required/><br /><br />
+    <input type="text" placeholder='YOUR PASSWORD' required /><br /><br />
     <button>SIGN UP</button>
     </form>
     </center>
